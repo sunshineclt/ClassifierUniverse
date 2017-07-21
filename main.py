7d9ae694d270aefef91abffc34d7ebe0158b835e
Zac Chan <sunshinecltzac@gmail.com>
2017-07-21 11:02:01 +0800

    ...

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ classifiers = [
     KNeighborsClassifier(3),
     SVC(kernel="linear", C=0.025),
     SVC(gamma=2, C=1),
-    # GaussianProcessClassifier(1.0 * RBF(1.0), warm_start=True),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
     MLPClassifier(alpha=1),
