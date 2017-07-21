50911886255fbc8ced0566ba276eeff5c405fd82
Zac Chan <sunshinecltzac@gmail.com>
2017-07-21 10:19:51 +0800

    整理

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.datasets import make_moons, make_circles, make_classification
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn import metrics
 from sklearn import datasets as skdatasets
+from sklearn import metrics
 from sklearn.datasets.mldata import fetch_mldata
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
-digits = fetch_mldata("MNIST original")
-number_of_data = digits.data.shape[0]
-number_of_small_data = number_of_data // 2
-number_of_cross_validation_data = number_of_data // 4
-number_of_big_data = number_of_data * 3 // 4
-number_of_test_data = number_of_data // 4
+digits_simple = skdatasets.load_digits()
+digits_full = fetch_mldata("MNIST original")
 
-train_data_small = digits.data[0:number_of_small_data]
-cross_validation_data = digits.data[number_of_small_data:number_of_small_data+number_of_cross_validation_data]
-train_data_big = digits.data[0:number_of_big_data]
-test_data = digits.data[number_of_big_data:number_of_big_data+number_of_test_data]
-train_label_small = digits.target[0:number_of_small_data]
-cross_validation_label = digits.target[number_of_small_data:number_of_small_data+number_of_cross_validation_data]
-train_label_big = digits.target[0:number_of_big_data]
-test_label = digits.target[number_of_big_data:number_of_big_data+number_of_test_data]
+# number_of_data = digits.data.shape[0]
+# number_of_small_data = number_of_data // 2
+# number_of_cross_validation_data = number_of_data // 4
+# number_of_big_data = number_of_data * 3 // 4
+# number_of_test_data = number_of_data // 4
+#
+# train_data_small = digits.data[0:number_of_small_data]
+# cross_validation_data = digits.data[number_of_small_data:number_of_small_data+number_of_cross_validation_data]
+# train_data_big = digits.data[0:number_of_big_data]
+# test_data = digits.data[number_of_big_data:number_of_big_data+number_of_test_data]
+# train_label_small = digits.target[0:number_of_small_data]
+# cross_validation_label = digits.target[number_of_small_data:number_of_small_data+number_of_cross_validation_data]
+# train_label_big = digits.target[0:number_of_big_data]
+# test_label = digits.target[number_of_big_data:number_of_big_data+number_of_test_data]
 
 names = ["Nearest Neighbors", "Linear SVM", "RBF SVM",
          "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
@@ -49,23 +45,9 @@ classifiers = [
     GaussianNB(),
     QuadraticDiscriminantAnalysis()]
 
-h = .02
+dataset_names = ["digits_simple", "digits_full"]
+datasets = [(digits_simple.data, digits_simple.target), (digits_full.data, digits_full.target)]
 
-X, y = make_classification(n_features=2, n_redundant=0, n_informative=2,
-                           random_state=1, n_clusters_per_class=1)
-rng = np.random.RandomState(2)
-X += 2 * rng.uniform(size=X.shape)
-linearly_separable = (X, y)
-
-# datasets = [make_moons(noise=0.3, random_state=0),
-#             make_circles(noise=0.2, factor=0.5, random_state=1),
-#             linearly_separable
-#             ]
-
-datasets = [(digits.data, digits.target)]
-
-# figure = plt.figure(figsize=(27, 9))
-i = 1
 # iterate over datasets
 for ds_cnt, ds in enumerate(datasets):
     # preprocess dataset, split into training and test part
@@ -76,75 +58,13 @@ for ds_cnt, ds in enumerate(datasets):
 
     x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
     y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
-                         np.arange(y_min, y_max, h))
-
-    # # just plot the dataset first
-    # cm = plt.cm.RdBu
-    # cm_bright = ListedColormap(['#FF0000', '#0000FF'])
-    # cm_dark = ListedColormap(['#00FFFF', '#FFFF00'])
-    # ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
-    # if ds_cnt == 0:
-    #     ax.set_title("Input data")
-    # # Plot the training points
-    # ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_dark)
-    # # and testing points
-    # ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6)
-    # ax.set_xlim(xx.min(), xx.max())
-    # ax.set_ylim(yy.min(), yy.max())
-    # ax.set_xticks(())
-    # ax.set_yticks(())
-    i += 1
 
     # iterate over classifiers
     for name, clf in zip(names, classifiers):
-        print("===================Now start %s===================" % (name))
-        # ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+        print("===================Now start %s for %s===================" % (name, dataset_names[ds_cnt]))
         print("fitting...")
         clf.fit(X_train, y_train)
-        # score = clf.score(X_test, y_test)
-
-        # Plot the decision boundary. For that, we will assign a color to each
-        # point in the mesh [x_min, x_max]x[y_min, y_max].
-        # if hasattr(clf, "decision_function"):
-        #     Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-        # else:
-        #     Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
         print("predicting...")
         predicted = clf.predict(X_test)
         print("evaluating...")
-        print("%s error: %d" % (name, metrics.accuracy_score(y_test, predicted)))
-
-        # Put the result into a color plot
-        # Z = Z.reshape(xx.shape)
-        # ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
-        #
-        # # Plot also the training points
-        # ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
-        # # and testing points
-        # ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,
-        #            alpha=0.6)
-        #
-        # ax.set_xlim(xx.min(), xx.max())
-        # ax.set_ylim(yy.min(), yy.max())
-        # ax.set_xticks(())
-        # ax.set_yticks(())
-        # if ds_cnt == 0:
-        #     ax.set_title(name)
-        # ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
-        #         size=15, horizontalalignment='right')
-
-        i += 1
-
-# plt.tight_layout()
-# plt.show()
-
-
-# clf = svm.SVC(gamma=0.001, C=100.)
-# clf.fit(train_data_big, train_label_big)
-#
-# train_predicted = clf.predict(train_data_big)
-# print("Train accuracy: %s" % (metrics.accuracy_score(train_label_big, train_predicted)))
-#
-# test_predicted = clf.predict(test_data)
-# print("Test accuracy: %s" % (metrics.accuracy_score(test_label, test_predicted)))
+        print("%s accuracy: %s" % (name, metrics.accuracy_score(y_test, predicted)))
